6ba8193cdcc78c5a41424a8203fb7347ade89e66
lacakush7800 <lavakushgiri@gmail.com>
2021-11-04 22:33:55 +0530

    add root

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\Crud\ProductController;
 
 /*
 |--------------------------------------------------------------------------
