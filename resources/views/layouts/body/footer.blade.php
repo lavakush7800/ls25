e4539374f05f0c32b853f96606512483503728d3
lacakush7800 <lavakushgiri@gmail.com>
2021-12-09 21:19:48 +0530

    about

--- resources/views/layouts/body/footer.blade.php
+++ resources/views/layouts/body/footer.blade.php
@@ -54,14 +54,14 @@
 
       <div class="mr-md-auto text-center text-md-left">
         <div class="copyright">
-          &copy; Copyright <strong><span>LS Store</span></strong>. All Rights Reserved
+          &copy; Copyright <strong><span>LS Tech Software Solutions</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
           <!-- All the links in the footer should remain intact. -->
           <!-- You can delete the links only if you purchased the pro version. -->
           <!-- Licensing information: https://bootstrapmade.com/license/ -->
           <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
-          Designed by <a href="#">LS</a>
+          <!-- Designed by <a href="#">LS</a> -->
         </div>
       </div>
       <div class="social-links text-center text-md-right pt-3 pt-md-0">
--- resources/views/layouts/body/header.blade.php
+++ resources/views/layouts/body/header.blade.php
@@ -2,13 +2,13 @@
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
 
-      <h1 class="logo mr-auto"><a href="/home"><span>LS </span>Tech</a></h1>
+      <h1 class="logo mr-auto"><a href="/"><span>LS </span>Tech</a></h1>
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.html">Home</a></li>
+          <li class="active"><a href="/">Home</a></li>
 
           <li class="drop-down"><a href="">About</a>
             <ul>
